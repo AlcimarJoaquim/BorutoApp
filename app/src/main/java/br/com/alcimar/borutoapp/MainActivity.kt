e5569ce143590499d7511250f252bd3aba741be7
Alcimar <alcimarjoaquim@outlook.com>
2023-10-17 17:34:30 -0300

    Setup Navigation

--- app/src/main/java/br/com/alcimar/borutoapp/MainActivity.kt
+++ app/src/main/java/br/com/alcimar/borutoapp/MainActivity.kt
@@ -3,13 +3,19 @@ package br.com.alcimar.borutoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import br.com.alcimar.borutoapp.navigation.SetupNavGraph
 import br.com.alcimar.borutoapp.ui.theme.BorutoAppTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoAppTheme {
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/br/com/alcimar/borutoapp/util/Constants.kt
@@ -0,0 +1,5 @@
+package br.com.alcimar.borutoapp.util
+
+object Constants {
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+}
