0826b710f24f66a7ccfa3144e45ec0e76d22deb9
Alcimar <alcimarjoaquim@outlook.com>
2023-10-18 16:55:19 -0300

    Create a database converter

--- app/src/main/java/br/com/alcimar/borutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/br/com/alcimar/borutoapp/data/local/BorutoDatabase.kt
@@ -2,12 +2,15 @@ package br.com.alcimar.borutoapp.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import br.com.alcimar.borutoapp.data.local.dao.HeroDao
 import br.com.alcimar.borutoapp.data.local.dao.HeroRemoteKeyDao
 import br.com.alcimar.borutoapp.domain.model.Hero
 import br.com.alcimar.borutoapp.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseCoverter::class)
 abstract class BorutoDatabase: RoomDatabase() {
     abstract fun heroDao(): HeroDao
     abstract  fun HeroRemoteKeyDao(): HeroRemoteKeyDao
--- app/src/main/java/br/com/alcimar/borutoapp/domain/model/HeroRemoteKey.kt
+++ app/src/main/java/br/com/alcimar/borutoapp/domain/model/HeroRemoteKey.kt
@@ -8,6 +8,6 @@ import br.com.alcimar.borutoapp.util.Constants.HERO_REMOTE_KEY_DATA
 data class HeroRemoteKey(
     @PrimaryKey(autoGenerate = false)
     val id: Int,
-    val prevKey: Int?,
-    val nextKey: Int?
+    val prevPage: Int?,
+    val nextPage: Int?
 )
